1d6ec8b8886b7047af14cab2e5e12c41f18a83d3
Alina-ps <alinaserg2014@gmail.com>
2024-07-17 05:54:48 +0200

    components folder added

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+
+import SearchBar from "../SearchBar/SearchBar";
+
+function App() {
+  return (
+    <>
+      <SearchBar />
+    </>
+  );
+}
+
+export default App;
