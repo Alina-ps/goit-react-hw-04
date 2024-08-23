7962b0536a1c13f45709a2a54bd1096a7a537469
Alina-ps <alinaserg2014@gmail.com>
2024-08-23 11:18:23 +0200

    corrected css

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,7 +1,7 @@
-import s from "./ImageCard.module.css";
+import s from './ImageCard.module.css';
 const ImageCard = ({ src, alt, onClick }) => {
   return (
-    <div onClick={onClick} className={s.imageCard}>
+    <div onClick={onClick}>
       <img src={src} alt={alt} className={s.image} />
     </div>
   );
